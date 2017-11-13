3f9e2120de5859a3392d2a02724340900f769368
bidzyyys <daniel.bigos96@gmail.com>
2017-11-13 21:58:59 +0100

    Added CSV Parser

---
+++ FileReader.cpp
@@ -0,0 +1,26 @@
+#include "FileReader.h"
+
+
+FileReader::FileReader()
+{
+}
+
+std::vector< std::string > FileReader::getFileRows( std::string file_name )
+{
+	std::vector< std::string > file_rows;
+	std::string row;
+	file_.open( file_name );
+	//first row - headers, no need to store them
+	std::getline( file_, row );
+	while ( std::getline( file_, row ) )
+		file_rows.push_back( row );
+
+	file_.close();
+	return file_rows;
+}
+
+FileReader::~FileReader()
+{
+	if( file_.is_open() )
+		file_.close();
+}
