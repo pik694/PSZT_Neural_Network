a47158ea578511e65c5ca5019d01fc35d70b77c7
mdziedzi <marcindziedzic8@gmail.com>
2018-01-07 00:47:17 +0100

    Rebuild Neuron class

--- Program/sources/neural_network/Neuron.cpp
+++ Program/sources/neural_network/Neuron.cpp
@@ -13,33 +13,38 @@ neural_network::Neuron::Neuron(std::function<double(double)> function, std::func
 }
 
 double neural_network::Neuron::getOutputValue() const {
-    return outputValue;
+    return output_value_;
 }
-
-void neural_network::Neuron::setOutputValue(double outputValue) {
-    Neuron::outputValue = outputValue;
-}
-
-void neural_network::Neuron::addInputSynapse(neural_network::Synapse inputSynapse) {
-    input_synapses_.push_back(inputSynapse);
-}
-
-void neural_network::Neuron::addOutputSynapse(neural_network::Synapse outputSynapse) {
-    output_synapses_.push_back(outputSynapse);
-
-}
-
 
 double neural_network::Neuron::sumInputs() {
     double sum = 0;
-    for (Synapse s : input_synapses_) {
-        sum += s.getValue();
+    for (const std::shared_ptr<Synapse> &s : input_synapses_) {
+        sum += s->getValue();
     }
     return sum;
 }
 
 double neural_network::Neuron::calculateOutputValue() {
-    return function_(sumInputs());
+    updateInputValue();
+    updateOutputValue();
+    return output_value_;
+}
+
+void neural_network::Neuron::addInputSynapse(const std::shared_ptr<neural_network::Synapse> &inputSynapse) {
+    input_synapses_.push_back(inputSynapse);
+}
+
+void neural_network::Neuron::addOutputSynapse(const std::shared_ptr<neural_network::Synapse> &outputSynapse) {
+    output_synapses_.push_back(outputSynapse);
+}
+
+void neural_network::Neuron::updateOutputValue() {
+    output_value_ = function_(input_value_);
+}
+
+void neural_network::Neuron::updateInputValue() {
+    input_value_ = sumInputs();
+
 }
 
 
