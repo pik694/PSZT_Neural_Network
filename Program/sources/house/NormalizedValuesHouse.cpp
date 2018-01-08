aff5b8688219a79bf0364b73399ff9e596968c47
Daniel Bigos <daniel@bidzyyys.bigos.com>
2018-01-08 21:02:18 +0100

    Added NormalizedValuesHouse

--- Program/sources/house/NormalizedValuesHouse.cpp
+++ Program/sources/house/NormalizedValuesHouse.cpp
@@ -1,13 +1,29 @@
-//
-// Created by Piotr Żelazko on 07.01.2018.
-//
-
 #include "NormalizedValuesHouse.h"
 
 house::NormalizedValuesHouse::NormalizedValuesHouse(const boost::gregorian::date &date, float price, int bedrooms,
                                                     float bathrooms, int sqftLiving, int sqftLot, float floors,
                                                     int waterfront, int view, int condition, int grade, int sqftAbove,
                                                     int sqftBasement, int yrBuilt, int yrRenovated, int zipcode,
-                                                    float lat, float f_long, int sqftLiving15, int sqftLot15) : House(
-		date, price, bedrooms, bathrooms, sqftLiving, sqftLot, floors, waterfront, view, condition, grade, sqftAbove,
-		sqftBasement, yrBuilt, yrRenovated, zipcode, lat, f_long, sqftLiving15, sqftLot15) {}
+                                                    float lat, float f_long, int sqftLiving15, int sqftLot15 ) {
+
+	date_ = ( ( date.year() * 10000 + date.month() * 100 + date.day() ) - MIN_DATE ) / (double)( MAX_DATE - MIN_DATE );
+	price_ = ( price - MIN_PRICE ) / (double) ( MAX_PRICE - MIN_PRICE );
+	bedrooms_ = ( bedrooms - MIN_BEDROOMS ) / (double) ( MAX_BEDROOMS - MIN_BEDROOMS );
+	bathrooms_ = ( bathrooms - MIN_BATHROOMS ) / (double) ( MAX_BATHROOMS - MIN_BATHROOMS );
+	sqftLiving_ = ( sqftLiving - MIN_SQFT_LIVING ) / (double) ( MAX_SQFT_LIVING - MIN_SQFT_LIVING );
+	sqftLot_ = ( sqftLot - MIN_SQFT_LOT ) / (double) ( MAX_SQFT_LOT - MIN_SQFT_LOT );
+	floors_ = ( floors - MIN_FLOORS ) / (double) ( MAX_FLOORS - MIN_FLOORS );
+	waterfront_ = ( waterfront - MIN_WATERFRONT ) / (double) ( MAX_WATERFRONT - MIN_WATERFRONT );
+	view_ = ( view - MIN_VIEW ) / (double) ( MAX_VIEW - MIN_VIEW );
+	sqftBasement_ = ( sqftBasement - MIN_SQFT_BASEMENT ) / (double) ( MAX_SQFT_BASEMENT - MIN_SQFT_BASEMENT );
+	yrBuilt_ = ( yrBuilt - MIN_YR_BUILT ) / (double) ( MAX_YR_BUILT - MIN_YR_BUILT );
+	condition_ = ( condition - MIN_CONDITION ) / (double) ( MAX_CONDITION - MIN_CONDITION );
+	grade_ = ( grade - MIN_GRADE ) / (double) ( MAX_GRADE - MIN_GRADE );
+	sqftAbove_ = ( sqftAbove - MIN_SQFT_ABOVE ) / (double) ( MAX_SQFT_ABOVE - MIN_SQFT_ABOVE );
+	yrRenovated_ = ( yrRenovated - MIN_YR_RENOVATED ) / (double) ( MAX_YR_RENOVATED - MIN_YR_RENOVATED );
+	zipcode_ = ( zipcode - MIN_ZIPCODE ) / (double) ( MAX_ZIPCODE - MIN_ZIPCODE );
+	latitude_ = ( lat - MIN_LATITUDE ) / (double) ( MAX_LATITUDE - MIN_LATITUDE );
+	longitude_ = ( f_long - MIN_LONGITUDE ) / (double) ( MAX_LONGITUDE - MIN_LONGITUDE );
+	sqftLiving15_ = ( sqftLiving15 - MIN_SQFT_LIVING15 ) / (double) ( MAX_SQFT_LIVING15 - MIN_SQFT_LIVING15 );
+	sqftLot15_ = ( sqftLot15 - MIN_SQFT_LOT15 ) / (double) ( MAX_SQFT_LOT15 - MIN_SQFT_LOT15 );
+}
--- Program/sources/house/NormalizedValuesHouse.h
+++ Program/sources/house/NormalizedValuesHouse.h
@@ -1,14 +1,53 @@
-//
-// Created by Piotr Żelazko on 07.01.2018.
-//
+// Created by Daniel Bigos
 
 #ifndef PSZT_NEURAL_NETWORK_NORMALIZEDVALUESHOUSE_H
 #define PSZT_NEURAL_NETWORK_NORMALIZEDVALUESHOUSE_H
 
 #include "House.h"
 
+const long MIN_DATE = 19000101;
+const long MAX_DATE = 21001231;
+const long MIN_PRICE = 0;
+const long MAX_PRICE = 50000000;
+const int MIN_BEDROOMS =  0;
+const int MAX_BEDROOMS = 100;
+const int MIN_BATHROOMS = 0;
+const int MAX_BATHROOMS = 20;
+const long MIN_SQFT_LIVING = 0;
+const long MAX_SQFT_LIVING = 100000;
+const long MIN_SQFT_LOT = 0;
+const long MAX_SQFT_LOT = 10000000;
+const int MIN_FLOORS = 0;
+const int MAX_FLOORS = 10;
+const int MIN_WATERFRONT = 0;
+const int MAX_WATERFRONT = 1;
+const int MIN_VIEW = 0;
+const int MAX_VIEW = 5;
+const int MIN_CONDITION = 0;
+const int MAX_CONDITION = 5;
+const int MIN_GRADE = 0;
+const int MAX_GRADE = 20;
+const int MIN_SQFT_ABOVE = 0;
+const int MAX_SQFT_ABOVE = 50000;
+const int MIN_SQFT_BASEMENT = 0;
+const int MAX_SQFT_BASEMENT = 20000;
+const int MIN_YR_BUILT = 1900;
+const int MAX_YR_BUILT = 2100;
+const int MIN_YR_RENOVATED = 1900;
+const int MAX_YR_RENOVATED = 2100;
+const long MIN_ZIPCODE = 0;
+const int MAX_ZIPCODE = 99999;
+const int MIN_LATITUDE = -180;
+const int MAX_LATITUDE = 180;
+const int MIN_LONGITUDE = -180;
+const int MAX_LONGITUDE = 180;
+const int MIN_SQFT_LIVING15 = 0;
+const int MAX_SQFT_LIVING15 = 20000;
+const int MIN_SQFT_LOT15 = 0;
+const long MAX_SQFT_LOT15 = 10000000;
+
 namespace house {
-	class NormalizedValuesHouse : public House {
+	class NormalizedValuesHouse {
 	public:
 		NormalizedValuesHouse(const boost::gregorian::date &date, float price, int bedrooms, float bathrooms,
 		                      int sqftLiving, int sqftLot, float floors, int waterfront, int view, int condition,
@@ -16,9 +55,68 @@ namespace house {
 		                      float lat, float f_long, int sqftLiving15, int sqftLot15);
 
 
+	private:
+		double date_;
+		double price_;
+		double bedrooms_;
+		double bathrooms_;
+		double sqftLiving_;
+		double sqftLot_;
+		double floors_;
+		double waterfront_;
+		double view_;
+		double condition_;
+		double grade_;
+		double sqftAbove_;
+		double sqftBasement_;
+		double yrBuilt_;
+		double yrRenovated_;
+		double zipcode_;
+		double latitude_;
+		double longitude_;
+		double sqftLiving15_;
+		double sqftLot15_;
 
-		// wszystkie wartosci maja sie miescic miedzy 0 a 1 (double)
-		// na chwile obecna taki sam interfejs co klasa House
+	public:
+		double getDate() const { return date_; }
+
+		double getPrice() const { return price_; }
+
+		double getBedrooms() const { return bedrooms_; }
+
+		double getBathrooms() const { return bathrooms_; }
+
+		double getSqftLiving() const { return sqftLiving_; }
+
+		double getSqftLot() const { return sqftLot_; }
+
+		double getFloors() const { return floors_; }
+
+		double getWaterfront() const { return waterfront_; }
+
+		double getView() const { return view_; }
+
+		double getCondition() const { return condition_; }
+
+		double getGrade() const { return grade_; }
+
+		double getSqftAbove() const { return sqftAbove_; }
+
+		double getSqftBasement() const { return sqftBasement_; }
+
+		double getYrBuilt() const { return yrBuilt_; }
+
+		double getYrRenovated() const { return yrRenovated_; }
+
+		double getZipcode() const { return zipcode_; }
+
+		double getLat() const { return latitude_; }
+
+		double getLong() const { return longitude_; }
+
+		double getSqftLiving15() const { return sqftLiving15_; }
+
+		double getSqftLot15() const { return sqftLot15_; }
 	};
 }
 
