99d6394f6e2eb3ea5d7a673dd2cc3454967214c5
Piotr <pik9603@gmail.com>
2018-01-08 22:33:58 +0100

    Added conversion between enum of Activation Functions and functions themselves.

--- Program/sources/neural_network/NeuralNetwork.h
+++ Program/sources/neural_network/NeuralNetwork.h
@@ -6,20 +6,20 @@
 
 #include "neural_network/neurons/Neuron.h"
 #include "house/NormalizedValuesHouse.h"
-
+#include "ActivationFunctionsBank.h"
 
 namespace neural_network {
 	class NeuralNetwork {
 	public:
-		NeuralNetwork(std::vector<int> neuronsInLayer,
-		              std::function<double(double)>  function,
-		              std::function<double(double)> derivative );
+		NeuralNetwork(std::vector<int> neuronsInLayer, functions::ActivationFunctions_E activationFunction);
 
 		double calculatePrice(const  NormalizedValuesHouse& house) {return feedForward(house);}
 
-		void propagateBack(const  NormalizedValuesHouse&);
-
 		double feedForward(const  NormalizedValuesHouse&);
+
+		void stochasticGradientDescent(const std::vector<NormalizedValuesHouse>& trainingData,
+		                               unsigned int epochs, unsigned int miniBatchSize, double eta,
+		                               std::function<void()> afterEachEpoch);
 
 		virtual ~NeuralNetwork() = default;
 
@@ -27,11 +27,15 @@ namespace neural_network {
 
 		using layer_t = std::vector<std::shared_ptr<neurons::Neuron>>;
 
+		void propagateBack(const  NormalizedValuesHouse&);
+
+
 
 		void setInputs(const  NormalizedValuesHouse&);
 		double getNetResult();
 
 		std::vector<layer_t> neurons_;
 
+		void updateWeights();
 	};
 }
