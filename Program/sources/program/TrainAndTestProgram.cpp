bfb6fa22df25055599a70eeea7a9627938050dd3
bidzyyys <daniel.bigos96@gmail.com>
2018-01-09 17:35:01 +0100

    Bug with execution mode

---
+++ Program/sources/program/TrainAndTestProgram.cpp
@@ -0,0 +1,9 @@
+#include "TrainAndTestProgram.h"
+
+using namespace program;
+
+void TrainAndTestProgram::run()
+{
+   // Program::run();
+}
+
--- Program/sources/program/program_initialization/ProgramInitializer.cpp
+++ Program/sources/program/program_initialization/ProgramInitializer.cpp
@@ -20,20 +20,25 @@ ProgramInitializer::ProgramInitializer(int argc, const char **argv) :
 		runOptions_ ("Program arguments"),
 		variablesMap_() {
 
+		//positionalOptions_.add( TRAIN.c_str(), -1 );
+		//positionalOptions_.add( TRAIN_AND_TEST.c_str(), -1 );
+		//positionalOptions_.add( TEST.c_str(), -1 );
 
 	infoOptions_.add_options()
-			(command(HELP, "h").c_str(), "Show this help message")
-			(command(VERSION, "v").c_str(), "Show program version");
+			( command( HELP, "h" ).c_str(), "Show this help message" )
+			( command( VERSION, "v" ).c_str(), "Show program version" );
 	runOptions_.add_options()
-			(command(FILE_IN, "i").c_str(), value<std::string>(&inputFileName)->required(), "Specifies input file");
-
+			( command( MODE, "m" ).c_str(), value< ExecutionMode_E >( &executionMode_ )->required(), "Specifies program mode")
+			( command( DATA, "d" ).c_str(), value< std::string >( &inputFileName_ )->required(), "Specifies input data file");
 }
 
-std::string ProgramInitializer::command(std::string longCommand, std::string shortCommand) const {
+std::string ProgramInitializer::command( std::string longCommand, std::string shortCommand ) const
+{
 	return (longCommand + "," + shortCommand);
 }
 
-std::unique_ptr<program::Program> ProgramInitializer::getProgram() {
+std::unique_ptr<program::Program> ProgramInitializer::getProgram()
+{
 
 	std::unique_ptr<Program> program = std::make_unique<Program>();
 	try {
@@ -62,11 +67,40 @@ std::unique_ptr<program::Program> ProgramInitializer::getProgram() {
 	}
 
 
+	try
+	{
+        switch ( executionMode_ )
+        {
+			case ExecutionMode_E::TRAIN:
+                return std::make_unique< TrainProgram >();
+            case ExecutionMode_E::TRAIN_AND_TEST:
+                return std::make_unique< TrainAndTestProgram >();
+            case ExecutionMode_E::TEST:
+                return std::make_unique< TestProgram >();
+            default:
+                throw std::runtime_error( "Unknown execution mode." );
+        }
 
-	try{
+        //example
+		/*if ( variablesMap_.count( TRAIN ) )
+		{
+			std::vector<std::string> options = variablesMap_[ TRAIN ].as<std::vector< std::string> >();
+			for ( std::string file : options )
+			{
+
+			}
+
+		}
+		}*/
+	}
+	catch (std::exception& e){
+		return std::make_unique<ErrorInfoProgram>(e.what(), std::move(program));
+	}
+
+	/*try{
 		FileReader fileReader;
-		ProgressStatusManager::getInstance()->init( "File reading: " + inputFileName );
-		std::vector< std::string > file_data = fileReader.getFileRows( inputFileName );
+		ProgressStatusManager::getInstance()->init( "File reading: " + inputFileName_ );
+		std::vector< std::string > file_data = fileReader.getFileRows( inputFileName_ );
 		ProgressStatusManager::getInstance()->deinit();
 
 		ProgressStatusManager::getInstance()->init( "Creating training data", file_data.size() );
@@ -75,15 +109,9 @@ std::unique_ptr<program::Program> ProgramInitializer::getProgram() {
 		training_data_factory.run( &file_data , &training_data );
 		ProgressStatusManager::getInstance()->deinit();
 	}
-	catch (std::invalid_argument& e){
+	catch (std::exception& e){
 		return std::make_unique<ErrorInfoProgram>(e.what(), std::move(program));
-	}
-	catch (std::runtime_error& e){
-		return std::make_unique<ErrorInfoProgram>(e.what(), std::move(program));
-	}
-	catch (std::bad_alloc& e){
-		return std::make_unique<ErrorInfoProgram>(e.what(), std::move(program));
-	}
+	}*/
 
 	return program;
 }
@@ -91,12 +119,12 @@ std::unique_ptr<program::Program> ProgramInitializer::getProgram() {
 void ProgramInitializer::parse() {
 
 	options_description all;
-	all.add(infoOptions_);
-	all.add(runOptions_);
+	all.add( infoOptions_ );
+	all.add( runOptions_ );
 
-	store(parse_command_line(argc_, argv_, all), variablesMap_);
+	store( parse_command_line( argc_, argv_, all ), variablesMap_ );
 
-	notify(variablesMap_);
+	notify( variablesMap_ );
 }
 
 
