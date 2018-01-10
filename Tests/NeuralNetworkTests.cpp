195b3a4393a8ef8d1b529e51eb11bb870f3b95a0
Piotr <pik9603@gmail.com>
2018-01-10 18:21:54 +0100

    Added neural network test suite

---
+++ Tests/NeuralNetworkTests.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Piotr Żelazko on 10.01.2018.
+//
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(NEURAL_NETWORK_TESTS)
+
+	BOOST_AUTO_TEST_CASE(NeuralNetworkCreation){
+
+		BOOST_CHECK_EQUAL(1,1);
+	}
+
+	BOOST_AUTO_TEST_CASE(){
+
+		BOOST_CHECK_EQUAL(1,1);
+	}
+
+	BOOST_AUTO_TEST_CASE(TopologyTest){
+
+		BOOST_CHECK_EQUAL(1,1);
+	}
+
+	BOOST_AUTO_TEST_CASE(NeuralNetworkCreation){
+
+		BOOST_CHECK_EQUAL(1,1);
+	}
+
+
+BOOST_AUTO_TEST_SUITE_END()
